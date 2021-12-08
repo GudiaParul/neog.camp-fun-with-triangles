ba816a351143fceeb3ffe392565f68a960e4eb6d
Parul Gupta <guptaparul1002@gmail.com>
2021-12-08 19:24:41 +0530

    f commit

--- area of triangle.js
+++ area of triangle.js
@@ -1,11 +1,11 @@
-var baseNHeight = document.querySelector(".sides");
+var baseNHeight = document.querySelectorAll(".sides");
 var btn = document.querySelector("#calculate-area");
 var output = document.querySelector("#output-area");
 
 
 function calculation(){
-    var base = Number(baseNHeight[0].value)
-    var height = Number(baseNHeight[1].value)
+    var base = Number(baseNHeight[0].value);
+    var height = Number(baseNHeight[1].value);
     return 0.5*base*height ;
 }
 
