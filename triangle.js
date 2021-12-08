170e55e9b89dc71370f3d95d9e4cd90e9ffff2b8
Parul Gupta <guptaparul1002@gmail.com>
2021-12-08 16:38:32 +0530

    final Is triangle commit

---
+++ triangle.js
@@ -0,0 +1,26 @@
+var inputs = document.querySelectorAll(".angle-input")
+var button = document.querySelector("#is-triangle-btn")
+var output = document.querySelector("#output")
+
+
+function calculateSum(Angle1,Angle2, Angle3){
+var angleSum = Angle1 + Angle2 + Angle3;
+return angleSum;
+}
+
+
+
+function formTriangle() {
+ var angleSum = calculateSum(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+    if (angleSum === 180){
+        console.log("Triangle is form");
+    } else {
+        console.log("Triangle is not form");
+    }
+    
+}
+
+
+
+
+button.addEventListener("click", formTriangle)
